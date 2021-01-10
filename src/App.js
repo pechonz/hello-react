8b6872b2087cb41b578870d4c27e175be932f159
Pechonz <77229678+pechonz@users.noreply.github.com>
2021-01-10 21:20:17 +0700

    21.1.10.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
-//Creat Component with JSX
+//Style and Expression with JSX
 class App extends Component {
   render(){
+    var mystyle = {
+      FontSize:100,
+      color:'#FF0000'
+    }
     return(
       <div>
-        <h1>Hello! React</h1>
+        <h1 style={mystyle}>Hello! React</h1>
+        <h2>{(50*50)/2}</h2>
       </div>
     );
   }
