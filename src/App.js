e4f17c3eb8940c1459863b2cb3634dd43487ec6b
Pechonz <77229678+pechonz@users.noreply.github.com>
2021-01-11 22:07:11 +0700

    21.1.11.2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import Header from './Header'
 import Content from './Content'
 import Footer from './Footer'
-//Props
+//State
 class App extends Component {
   render(){
     return(
--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,25 @@ import React, {Component} from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
 class Header extends Component {
+  constructor(){
+    super();
+    this.state={
+      name:"Naruephon Sooksinchai",
+      count:0
+    };
+  }
   render(){
+    setTimeout(()=>{
+        this.setState({name:"Petch"});
+    },1000);
+    setInterval(()=>{
+        this.setState({count:this.state.count+1});
+    },1000);
     return(
       <div class="jumbotron text-center">
         <h1>My First Bootstrap Page</h1>
+        <h2>{this.state.name}</h2>
+        <h2>{this.state.count}</h2>
         <p>Resize this responsive page to see the effect!</p>
       </div>
     );
